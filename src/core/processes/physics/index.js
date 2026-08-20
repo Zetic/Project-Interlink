02f9624c00d0d33a153af4afddb015daddf770ba
Zetic <105380590+Zetic@users.noreply.github.com>
2026-08-20 13:53:58 -0400

    feat: export routing physics kernels

--- src/core/processes/physics/index.js
+++ src/core/processes/physics/index.js
@@ -2,8 +2,11 @@ export {
   crushSolidMaterialState,
   hasCrushableSolidFractions,
 } from './crushing.js';
+export { feedSolidMaterialState } from './feeding.js';
+export { mergeSolidMaterialStates } from './merging.js';
 export {
   magneticRecoveryForFraction,
   splitMagneticSolidState,
 } from './magneticSeparation.js';
 export { splitScreenedSolidState } from './screening.js';
+export { splitSolidMaterialState } from './splitting.js';
